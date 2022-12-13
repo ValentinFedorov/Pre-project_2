c46b4d7deaa5e004b816672f0cd66fac250895ab
ValentinFedorov <106467201+ValentinFedorov@users.noreply.github.com>
2022-12-13 20:18:59 +0700

    Add files via upload

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+        (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Ссылки на Бины HelloWorld равны? " + (bean == bean2) + "\n" +
+                "Ссылки на Бины Cat равны? " + (catBean1 == catBean2));
+    }
+}
